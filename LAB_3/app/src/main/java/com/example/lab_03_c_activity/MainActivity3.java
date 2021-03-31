560cc9fcada2fc52560053db751f553e9d6a9317
=nguyenvantrung44 <=nguyenvantrung4424121999@gmail.com>
2021-03-31 18:05:23 +0700

    update 2b

---
+++ LAB_3/app/src/main/java/com/example/lab_03_c_activity/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.lab_03_c_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
